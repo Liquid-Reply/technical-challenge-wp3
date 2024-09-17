298bfa1eedb23bccc082bf8fcb6e754eddb3ba22
Fabian Brundke <f.brundke@reply.de>
2024-09-17 10:43:49 +0200

    lower vm instance type due to quota limitations

--- infra/compute.tf
+++ infra/compute.tf
@@ -5,7 +5,7 @@ resource "azurerm_linux_virtual_machine" "keycloak" {
   location              = local.location
   resource_group_name   = local.resource_group_name
   network_interface_ids = [azurerm_network_interface.keycloak[each.key].id]
-  size                  = "Standard_D4_v5"
+  size                  = "Standard_D2_v5"
   #TODO: encryption
 
   os_disk { #TODO: check setting
