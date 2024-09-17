2c3dd19136d70b3deac338f6c513ec165e1820cc
Jagdeep Manjunatha <j.manjunatha@reply.de>
2024-09-17 13:53:50 +0200

    add inital AKS setup

--- infra/main.tf
+++ infra/main.tf
@@ -2,3 +2,40 @@ resource "azurerm_resource_group" "main" {
   name = "tech-challenge"
   location = local.location
 }
+
+
+resource "random_pet" "azurerm_kubernetes_cluster_name" {
+  prefix = "cluster"
+}
+
+resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
+  prefix = "dns"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  location            = azurerm_resource_group.main.location
+  name                = random_pet.azurerm_kubernetes_cluster_name.id
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  kubernetes_version    = var.kubernetes_version
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D8s_v5"
+    node_count = var.node_count
+  }
+  linux_profile {
+    admin_username = var.username
+
+    ssh_key {
+      key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
+    }
+  }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+}
--- infra/terraform.tf
+++ infra/terraform.tf
@@ -4,6 +4,21 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>4.0"
     }
+
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+
   }
   backend "azurerm" {
     resource_group_name  = "tfstate"
