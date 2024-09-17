d8e870a90419749de4cf1137d7c27d0cc8f5a822
Fabian Brundke <f.brundke@reply.de>
2024-09-17 10:54:53 +0200

    remove vm setup

--- infra/compute.tf
+++ infra/compute.tf
@@ -1,29 +1,29 @@
-#TODO: maybe switch to scale set
-resource "azurerm_linux_virtual_machine" "keycloak" {
-  for_each = toset(local.zones)
-  name                  = "keycloak-${each.key}"
-  location              = local.location
-  resource_group_name   = local.resource_group_name
-  network_interface_ids = [azurerm_network_interface.keycloak[each.key].id]
-  size                  = "Standard_D2_v5"
-  #TODO: encryption
+# #TODO: maybe switch to scale set
+# resource "azurerm_linux_virtual_machine" "keycloak" {
+#   for_each = toset(local.zones)
+#   name                  = "keycloak-${each.key}"
+#   location              = local.location
+#   resource_group_name   = local.resource_group_name
+#   network_interface_ids = [azurerm_network_interface.keycloak[each.key].id]
+#   size                  = "Standard_D2_v5"
+#   #TODO: encryption
 
-  os_disk { #TODO: check setting
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS" 
-  }
+#   os_disk { #TODO: check setting
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS" 
+#   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts-gen2"
+#     version   = "latest"
+#   }
 
-  admin_username = local.admin_username
+#   admin_username = local.admin_username
 
-  admin_ssh_key {
-    username   = local.admin_username
-    public_key = file("./files/id_rsa_keycloak.pub")
-  }
-}
+#   admin_ssh_key {
+#     username   = local.admin_username
+#     public_key = file("./files/id_rsa_keycloak.pub")
+#   }
+# }
--- infra/network.tf
+++ infra/network.tf
@@ -43,58 +43,57 @@ resource "azurerm_nat_gateway_public_ip_association" "main" {
   public_ip_address_id = azurerm_public_ip.nat[each.key].id
 }
 
-# Associate NAT Gateway with Subnet
 resource "azurerm_subnet_nat_gateway_association" "main" {
   for_each = toset(local.zones)
   subnet_id      = azurerm_subnet.keycloak[each.key].id
   nat_gateway_id = azurerm_nat_gateway.main[each.key].id
 }
 
-resource "azurerm_public_ip" "keycloak" {
-  for_each = toset(local.zones)
-  name     = "keycloak-${each.key}"
+# resource "azurerm_public_ip" "keycloak" {
+#   for_each = toset(local.zones)
+#   name     = "keycloak-${each.key}"
 
-  allocation_method   = "Static"
-  location            = local.location
-  resource_group_name = local.resource_group_name
-  sku                 = "Standard"
-  zones               = [each.key]
-}
+#   allocation_method   = "Static"
+#   location            = local.location
+#   resource_group_name = local.resource_group_name
+#   sku                 = "Standard"
+#   zones               = [each.key]
+# }
 
-resource "azurerm_network_security_group" "keycloak" {
-  name                = "keycloak"
-  location            = local.location
-  resource_group_name = local.resource_group_name
+# resource "azurerm_network_security_group" "keycloak" {
+#   name                = "keycloak"
+#   location            = local.location
+#   resource_group_name = local.resource_group_name
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
+#   security_rule {
+#     name                       = "SSH"
+#     priority                   = 1001
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "22"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+# }
 
-resource "azurerm_network_interface" "keycloak" {
-  for_each = toset(local.zones)
-  name                = "keycloak-${each.key}"
-  location            = local.location
-  resource_group_name = local.resource_group_name
+# resource "azurerm_network_interface" "keycloak" {
+#   for_each = toset(local.zones)
+#   name                = "keycloak-${each.key}"
+#   location            = local.location
+#   resource_group_name = local.resource_group_name
 
-  ip_configuration {
-    name                          = "keycloak"
-    subnet_id                     = azurerm_subnet.keycloak[each.key].id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.keycloak[each.key].id
-  }
-}
+#   ip_configuration {
+#     name                          = "keycloak"
+#     subnet_id                     = azurerm_subnet.keycloak[each.key].id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.keycloak[each.key].id
+#   }
+# }
 
-resource "azurerm_network_interface_security_group_association" "keycloak" {
-  for_each = toset(local.zones)
-  network_interface_id      = azurerm_network_interface.keycloak[each.key].id
-  network_security_group_id = azurerm_network_security_group.keycloak.id
-}
+# resource "azurerm_network_interface_security_group_association" "keycloak" {
+#   for_each = toset(local.zones)
+#   network_interface_id      = azurerm_network_interface.keycloak[each.key].id
+#   network_security_group_id = azurerm_network_security_group.keycloak.id
+# }
